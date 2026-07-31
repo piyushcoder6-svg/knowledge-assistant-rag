2c3e6084275fbe89f8ecde40ccce9a389d2d583c
piyushcoder6-svg <piyushcoder6@gmail.com>
2026-08-01 00:18:10 +0530

    Update ingest.py

--- backend/ingest.py
+++ backend/ingest.py
@@ -1,6 +1,7 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEndpointEmbeddings
 from langchain_chroma import Chroma
 from dotenv import load_dotenv
 
@@ -14,8 +15,9 @@ def ingest_pdf(pdf_path: str):
         chunk_overlap=50
     )
     chunks = splitter.split_documents(documents)
-    embeddings = HuggingFaceEmbeddings(
-        model_name="all-MiniLM-L6-v2"
+    embeddings = HuggingFaceEndpointEmbeddings(
+        model="sentence-transformers/all-MiniLM-L6-v2",
+        task="feature-extraction",
     )
     vectorstore = Chroma.from_documents(
         chunks,
